b96bf563d3b1b75d4ff32f11ef460c635a0600fe
Phil Silveira <phills.silveira@gmail.com>
2023-08-27 20:06:49 -0300

    ✨ realizando desafio dio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nomeCompleto: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val html = ConteudoEducacional("Html")
+    val css = ConteudoEducacional("CSS")
+    val js = ConteudoEducacional("JavaScript")
+
+    val formacaoFrontend = Formacao(
+        "Formação Frontend", 
+        Nivel.BASICO, 
+        listOf(html, css, js)
+    )
+
+    val tania = Usuario("Tania Silva")
+    val claudio = Usuario("Claudio Duarte")
+
+    formacaoFrontend.matricular(tania)
+    formacaoFrontend.matricular(claudio)
+
+    print("Formação deve ter 2 inscritos - ${formacaoFrontend.inscritos.count() == 2}")
 }
